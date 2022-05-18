9af03ce827460be9c273f81443e0bdc4573d58f5
Marzan1810662 <hassanmarzan17@gmail.com>
2022-05-19 05:50:50 +0600

    jwt implemented

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
@@ -12,7 +13,23 @@ app.use(cors())
 app.use(express.json())
 
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
 
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,
+        function (err, decoded) {
+            if (err) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+            req.decoded = decoded;
+            next();
+        });
+
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wiok4.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -21,10 +38,18 @@ async function run() {
     try {
         await client.connect();
         const taskCollection = client.db('todo_app').collection('tasks')
+        
+        app.post('/token', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '2d'
+            });
+            res.send({ accessToken });
+        })
 
-        app.get('/task/:email', async (req, res) => {
+        app.get('/task/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
-            const query = {email:email};
+            const query = { email: email };
             const tasks = await taskCollection.find(query).toArray()
             res.send(tasks)
         })
@@ -37,7 +62,7 @@ async function run() {
 
         app.put('/task/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = { _id : ObjectId(id)};
+            const filter = { _id: ObjectId(id) };
             const updateDoc = {
                 $set: { status: 'done' },
             };
@@ -47,7 +72,7 @@ async function run() {
 
         app.delete('/task/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id : ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await taskCollection.deleteOne(query);
             res.send(result);
         })
