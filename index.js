28ae5e54e41eef3183410377f39bb0cf39bc6555
Marzan1810662 <hassanmarzan17@gmail.com>
2022-05-19 00:37:45 +0600

    root api and MongoDB connection

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wiok4.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const taskCollection = client.db('todo_app').collection('tasks')
+
+        app.get('/task', async (req, res) => {
+            res.send('todo app connected to mongo db')
+        })
+
+        app.post('/task',async (req, res) => {
+            const task = req.body; 
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send("To do app server")
+})
+
+app.listen(port, () => {
+    console.log('to do app running on port', port);
+})
+
